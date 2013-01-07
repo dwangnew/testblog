e4afb87134f0adb9b4b35368fa7fdff131d1fc00
DavidWang12 <nosfer678@gmail.com>
2013-01-06 23:18:34 -0500

    Generate static pages controller and static pages try#2

---
+++ app/controllers/staticpages_controller.rb
@@ -0,0 +1,19 @@
+class StaticpagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def links
+  end
+
+  def contact
+  end
+
+  def projects
+  end
+
+  def blog
+  end
+end
